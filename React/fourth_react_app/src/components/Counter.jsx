b3bc5153413583459e78217608d702538dff7d40
CSESCEM <office.cs@sahyadri.edu.in>
2025-08-05 14:37:24 +0530

    Initial commit

---
+++ React/fourth_react_app/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+// components/Counter.jsx
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Counter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
