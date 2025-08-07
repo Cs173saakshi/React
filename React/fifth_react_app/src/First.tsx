b4654af01bf39de378ab26f2d3c311b49412dfce
CSESCEM <office.cs@sahyadri.edu.in>
2025-08-07 17:02:59 +0530

    first commit

---
+++ React/fifth_react_app/src/First.tsx
@@ -0,0 +1,13 @@
+function First(){
+let name:string="Saakshi";
+let age:number=21;
+
+    return(
+        <div>
+<h1>React with JavaScript</h1>
+<p>Name:{name}</p>
+<p>Age:{age}</p>
+        </div>
+    )
+}
+export default First;
