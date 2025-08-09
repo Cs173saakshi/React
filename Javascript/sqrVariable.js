32313e002263cd3287d457c18686de50601c62d1
CSESCEM <office.cs@sahyadri.edu.in>
2025-08-09 17:03:41 +0530

    new commit

--- Javascript/sqrVariable.js
+++ Javascript/sqrVariable.js
@@ -13,7 +13,6 @@ function evaluateExpression(expr, vars, vals) {
   }
 }
 
-// Test cases
 console.log(evaluateExpression("sqrt(a - b) + c", ["a", "b", "c"], [9, 4, 3]));     
 console.log(evaluateExpression("sqrt(x * y) - z", ["x", "y", "z"], [2, -3, 1]));   
 console.log(evaluateExpression("sqrt(m) + sqrt(n)", ["m", "n"], [16, 9]));        
---
+++ React/new_test_app/src/tests/App.test.jsx
@@ -0,0 +1,10 @@
+import {render,screen} from "@testing-library/react"
+
+import App from "./App";
+
+test("App Test :",()=>{
+    render(<App/>);
+
+    const headingElement=screen.getByText(/React with testing.../i);
+    expect(headingElement).toBeInTheDocument();
+})
